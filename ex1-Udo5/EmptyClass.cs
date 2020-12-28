8230d6bafa0b175506b13c4b74e6a4c5f7897b63
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-28 19:44:29 +0100

    ex1-udo5

---
+++ ex1-Udo5/EmptyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ex1_Udo5
+{
+    public class Circulo
+    {
+        public Circulo(double radioCirculo)
+        {
+            static double areaCirculo(double radioCirculo);
+            return Math.Pow(radioCirculo, 2) * Math.PI;
+        }
+    }
+}
---
+++ ex1-Udo5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex1_Udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int areacirculo, areatriangulo, areacuadrado;
+
+            Console.WriteLine("Elije una figura geometrica");
+            Console.WriteLine("1.Cuadrado");
+            Console.WriteLine("2.Rectangulo");
+            Console.WriteLine("3.Cuadrado");
+
+            
+        }
+    }
+}
