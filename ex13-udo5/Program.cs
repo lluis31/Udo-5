528901070787b43ca2471bb2e0776b214e301865
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 13:10:03 +0100

    ex13-udo5

---
+++ ex13-udo5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex13_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe cuantos numeros aleatorios del 1 al 10 quieres crear:");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+
+            int[] numeros = new int[cantidad];
+            for (int i = 0; i < cantidad; i ++)
+            {
+                numeros[i] = r.Next(0, 11);
+                Console.WriteLine("Posicion {0}: valor {1}", i + 1, numeros[i]);
+            }
+        }
+    }
+}
