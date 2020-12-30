97b52267d73722f22233012f4478757e5e68aa2c
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 13:57:55 +0100

    ex17-udo5

---
+++ ex17-udo5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex17_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad = 10;
+            int[] numeros = new int[cantidad];
+
+            for (int i =0; i < cantidad; i++)
+            {
+                Console.WriteLine("Escribe el valor de la posicion {0}", i + 1);
+
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("");
+            Mostrar(numeros, cantidad);
+
+        }
+        public void Mostrar(int[] numeros, int cantidad)
+        {
+            for (int i =0; i < cantidad; i++)
+            {
+                Console.WriteLine("El valor de la posicion {0} es {1}", i + 1, numeros[i]);
+            }
+        }
+    }
+}
