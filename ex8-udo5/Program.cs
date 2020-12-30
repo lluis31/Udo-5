1299b6e493d483e854965ebed2352faabcbf14fc
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:41:14 +0100

    ex8-udo5

---
+++ ex8-udo5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex8_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantas posiciones tiene el array:");
+
+            int posicion = Convert.ToInt32(Console.ReadLine());
+            int[] exercici8 = new int[posicion];
+
+            for (int i = 0; i < posicion; i++)
+            {
+                exercici8[i] = i;
+                Console.WriteLine("{0}", exercici8[i]);
+            }
+        }
+    }
+}
