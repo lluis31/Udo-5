3187c8b8c0b0330f02afccc74091289d643c026a
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 14:29:20 +0100

    ex17-udo5

---
+++ ex17-udo5/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ex17_udo5
+{
+    public class EmptyClass
+    {
+        public void Mostrar(int[] numeros, int cantidad)
+        {
+
+        }
+    }
+}
---
+++ ex18-udo5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex18_udo5
+{
+    class Program
+
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe cuantos numeros aleatorios del 1 al 9 quieres crear:");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[cantidad];
+            Rellenar(numeros, cantidad);
+
+        }
+        public void Rellenar(int[] numeros, int cantidad)
+        {
+            Random r = new Random();
+
+            int total = 0;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                numeros[i] = r.Next(1, 11);
+                total = total + numeros[i];
+
+                Console.WriteLine("El valor de la posición {0} es {1}", i + 1, numeros[i]);
+            }
+            Console.WriteLine("La suma total es:{0}", total);
+        }
+    }
+}
