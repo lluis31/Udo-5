d7d9cdd31ad4ab6ac4f1c1e23f472b34c611a1c4
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:48:46 +0100

    ex9-udo5

---
+++ ex9-udo5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex9_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantas posiciones quieres que tenga el array,");
+
+            int posicion = Convert.ToInt32(Console.ReadLine());
+            int[] exercici9 = new int[posicion];
+
+            for (int i = 0; i < posicion; i++)
+            {
+                Console.WriteLine("Escribe el valor de la posicion {0}", i);
+                exercici9[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i< posicion; i++)
+            {
+                Console.WriteLine("{0}", exercici9[i]);
+            }
+        }
+    }
+}
