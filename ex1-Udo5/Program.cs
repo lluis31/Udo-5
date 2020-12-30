54f3c353367aa769fd1ae84d061930a6e794262e
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 09:37:46 +0100

    ex1

--- ex1-Udo5/Program.cs
+++ ex1-Udo5/Program.cs
@@ -1,29 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace ex1_Udo5
+namespace UD5Ex1al21
 {
-    class Program
+    class Ex1
     {
-        public void cuadrado()
+        public void Inicio()
         {
-            int lado, area;
-            Console.WriteLine("Ingresa un lado del cuadrado");
-            lado = Convert.ToInt16(Console.ReadLine());
-            area = lado * lado;
-            Console.WriteLine("El area del cuadrado es:" + area);
-            Console.ReadKey();
-        }
+            double area = 0;
+            int basee = 0;
+            int altura = 0;
+            Console.WriteLine("Que área quiere calcular: Circulo, Triangulo o Cuadrado?");
+            string figura = Console.ReadLine();
+            switch (figura)
+            {
+                case "Circulo":
+                    Console.WriteLine("Introduce el radio del circulo:");
+                    area = Math.PI * Math.Pow(Convert.ToInt32(Console.ReadLine()), 2);
+                    break;
 
-        public void circulo()
-        {
-            double radio, Math.Pi, area;
+                case "Triangulo":
+                    Console.WriteLine("Introduce la base del triangulo");
+                    basee = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Introduce la altura del triangulo:");
+                    altura = Convert.ToInt32(Console.ReadLine());
+                    area = basee * altura / 2;
+                    break;
 
-        }
-        static void Main(string[] args)
-        {
-            
-
+                case "Cuadrado":
+                    Console.WriteLine("Introduce el lado del cuadrado:");
+                    area = Math.Pow(Convert.ToInt32(Console.ReadLine()), 2);
+                    break;
             }
+            Console.WriteLine("El area de tu figura es {0}", area);
         }
     }
 }
