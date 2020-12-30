de6f309c82bac37cbad1e6befca448020a30ef0d
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 13:24:09 +0100

    ex15-udo5

---
+++ ex15-udo5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex15_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe cuantos numeros tiene el array");
+
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            int[] numeros = new int[cantidad];
+            int[] numeros2 = new int[cantidad];
+
+            for (int i = 0; i< cantidad; i++)
+            {
+                Console.WriteLine("Escribe el valor de la posicion {0}", i + 1);
+
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                numeros2[cantidad - 1 - i] = numeros[i];
+            }
+            Console.WriteLine("");
+
+            for (int i = 0; i< cantidad; i ++)
+            {
+                Console.WriteLine("Posicion {0}: valor {1}", i + 1, numeros2[i]);
+            }
+        }
+    }
+}
