f0084b0a8e25f9c5a67c021bf881188a8e022eb7
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:09:34 +0100

    ex5-udo5

---
+++ ex5-udo5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ex5_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero >0)
+            {
+                String cadena = "";
+                while (numero >0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        cadena = "0" + cadena;
+                    }
+                    else
+                    {
+                        cadena = "1" + cadena;
+                    }
+                    numero = (int)(numero / 2);
+                }
+                Console.WriteLine("Resultado en binario es: {0}", cadena);
+            }
+            else
+            {
+                if (numero == 0)
+                {
+                    Console.WriteLine("0");
+                }
+            else
+                {
+                    Console.WriteLine("Escribs solo numeros positivos");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
