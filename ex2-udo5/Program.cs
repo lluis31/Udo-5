13ad19203ab50ac9d67466e0472f3e46cfd8c2be
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 11:28:25 +0100

    ex2-udo5

---
+++ ex2-udo5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex2_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce cuantos numeros aleatorios quieres generar:");
+
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el limite inferior del rango de aleatoriedad:");
+
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el limite superior del rango de aleatoriedad:");
+
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+
+            int[] numeros = new int[cantidad];
+            for (int i = 0; i < cantidad; i++)
+            {
+                numeros[i] = r.Next(min - 1, max + 1);
+                Console.WriteLine("Posición {0}: Valor {1}", i + 1, numeros[i]);
+            }
+        }
+    }
+}
