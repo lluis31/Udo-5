a76d4160f9fd9e6e84a7b293ae32e6ad878dafa8
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 13:04:02 +0100

    ex12-udo5

---
+++ ex12-udo5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex12_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Escribe el valor de la posicion {0}", i + 1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i<10; i ++)
+            {
+                array[0] = array[0] + array[i];
+            }
+            Console.WriteLine("La media es de {0}", array[0] /10);
+        }
+    }
+}
