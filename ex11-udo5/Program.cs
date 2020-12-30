ea61bf34bdc1e0b417a99344cc6dcb5a8adfc4a9
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:58:21 +0100

    ex11-udo5

---
+++ ex11-udo5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex11_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i=0; i<10; i++)
+            {
+                Console.WriteLine("Escribe el valor de la posicion {0}", i + 1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i<10; i++)
+            {
+                array[0] = array[0] + array[i];
+            }
+            Console.WriteLine("Suma total es de {0}", array[0]);
+        }
+    }
+}
