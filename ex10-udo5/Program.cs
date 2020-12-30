081b4efff8eb1349459f976b0d78dd71582aa016
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:53:07 +0100

    ex10-udo5

---
+++ ex10-udo5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex10_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escoge una tabla de multiplicar del 1 a 10:");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i< 11; i ++)
+            {
+                Console.WriteLine("{0}*{1}={2}", x, i, x * i);
+            }
+        }
+    }
+}
