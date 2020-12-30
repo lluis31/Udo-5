c4746f6df02157d24c976f2aded82d4b5fc2c21a
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 12:20:12 +0100

    ex6-udo5

---
+++ ex6-udo5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex6_udo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un numero entero decimal:");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int decimales = Convert.ToInt32(Math.Floor(Math.Log(num) + 1));
+
+            Console.WriteLine("El numero {0} tiene {1} digitos", num, decimales);
+        }
+    }
+}
